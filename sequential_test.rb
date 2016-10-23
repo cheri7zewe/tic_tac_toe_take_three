0548a1e62bd0550eb53d0be2a77c7deb5e244613
cheri7zewe <cheri.zewe@gmail.com>
2016-10-23 16:44:16 -0400

    sequential ai

---
+++ sequential_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require_relative "sequential.rb"
+
+class TestSequentialAI < Minitest::Test
+
+	def test_first_available_space_returned
+        player = SequentialAI.new("X")
+        assert_equal(0, player.get_move([" "," "," "," "," "," "," "," "," "]))
+        assert_equal(2, player.get_move(["O","O"," "," "," "," "," "," "," "]))
+    	assert_equal(4, player.get_move(["O","O","X","X"," "," "," "," "," "]))
+    end
+
+    def test_for_creating_player
+        player = SequentialAI.new("X")
+        assert_equal("X", player.marker)
+    end
+
+end
